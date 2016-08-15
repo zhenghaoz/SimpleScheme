80e830785972226d6b6e4d6645faf58428751a36
ZhangZhenghao <zhangzhenghao@hotmail.com>
2016-08-15 14:09:36 +0800

    Refactor evaluator loop

--- main.cpp
+++ main.cpp
@@ -10,21 +10,29 @@
 using namespace std;
 using namespace eva;
 
+#define EVAL_LOOP(in) do {\
+	Environment env = setupEnvironment();\
+	GarbageCollector::setGlobalEnvironment(env);\
+	while (true) {\
+		try {\
+			Variable exp;\
+			if (!(in >> exp))\
+				break;\
+			Variable val = eval(exp, env);\
+			if (!val.isVoid())\
+				cout << val << endl;\
+		} catch (SchemeException e) {\
+			e.printStack();\
+		}\
+		GarbageCollector::collect();\
+	}\
+} while (0)
+
 int main(int argc, char const *argv[])
 {
-	Environment env = setupEnvironment();
-	while (true) {
-		try {
-			Variable exp;
-			if (!(cin >> exp))
-				return EOF;
-				Variable val = eval(exp, env);
-				if (!val.isVoid())
-					cout << val << endl;
-		} catch (SchemeException e) {
-			e.printStack();
-		}
-		GarbageCollector::collect();
-	}
+	if (argc > 1)
+		EVAL_LOOP(fstream(argv[1]));
+	else
+		EVAL_LOOP(cin);	
 	return 0;
 }
