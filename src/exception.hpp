1008f19e0f5b305f6f5941273cbbf30c594bc55a
ZhangZhenghao <zhangzhenghao@hotmail.com>
2016-11-13 19:53:32 +0800

    Refractor code

--- src/exception.hpp
+++ src/exception.hpp
@@ -1,30 +1,43 @@
-#ifndef EXCEPTION_H
-#define EXCEPTION_H
+// 
+// Exception for Scheme
+// 
+// Author: Zhang Zhenghao (zhangzhenghao@hotmail.com)
+// 
+#pragma once
 
 #include <string>
 #include <vector>
 
-EVAL_NAMESPACE_BEGIN
-
-class SchemeException
+class Exception
 {
-	std::string _msg;
-	std::vector<std::string> _trace;
+	// Type alias
+	using string = std::string;
+	using vector = std::vector<string>;
+
+	// Data member
+	string msg;
+	vector trace;
+
 public:
-	SchemeException(std::string msg): _msg(msg) {}
-	void addTrace(const std::string &exp) { _trace.push_back(exp); }
+
+	// Constructor
+	Exception(const string& msg): msg(msg) {}
+
+	void addTrace(const string& exp) 
+	{ 
+		trace.push_back(exp);
+	}
+
 	void printStack()
 	{
-		std::cout << _msg << std::endl;
-		int length = _trace.size();
-		for (int i = 0; i < length; i++)
+		using std::cout;
+		using std::endl;
+
+		cout << msg << endl;
+		for (int i = 0; i < trace.size(); i++)
 			if (i == 0)
-				std::cout << "\tfrom: " << _trace[i] << std::endl;
+				cout << "\tfrom: " << trace[i] << endl;
 			else
-				std::cout << "\t      " << _trace[i] << std::endl;
+				cout << "\t      " << trace[i] << endl;
 	}
 };
-
-EVAL_NAMESPACE_END
-
-#endif
