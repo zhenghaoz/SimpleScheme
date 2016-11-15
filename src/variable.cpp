18ed35bcfa2a97708c9a639b348ea1ff4d5d64e2
ZhangZhenghao <zhangzhenghao@hotmail.com>
2016-11-15 16:17:37 +0800

    Refactor variable module

--- src/variable.cpp
+++ src/variable.cpp
@@ -1,345 +1,118 @@
+//
+// Low level variable and environment for Scheme
+//
+// Author: Zhang Zhenghao (zhangzhenghao@hotmail.com)
+//
+#include <iostream>
 #include "variable.hpp"
-#include "exception.hpp"
 
-EVAL_NAMESPACE_BEGIN
+// Type alias
+using ostream = std::ostream;
 
-/* garbage collector */
-
-default_random_engine GarbageCollector::engine;
-uniform_int_distribution<int> GarbageCollector::distribution;
-vector<Variable> GarbageCollector::_watchList = vector<Variable>();
-Environment GarbageCollector::_globalEnv;
-
-void GarbageCollector::setGlobalEnvironment(const Environment& env) { _globalEnv = env; }
-
-void GarbageCollector::addVar(const Variable& var) { _watchList.push_back(var); }
-
-void GarbageCollector::collect()
+// Standard output
+ostream& operator<<(ostream& out, const Variable& var)
 {
-	int tag = distribution(engine);
-	_globalEnv.setTag(tag);
-	vector<Variable> nWatchList;
-	for (Variable& var : _watchList)
-		if (*var._refCount > 1 && *var._tag != tag)
-			var.finalize();
-		else if (*var._refCount > 1)
-			nWatchList.push_back(var);
-	std::swap(_watchList, nWatchList);
-}
-
-/* primitive procedure */
-
-PrimitiveProcdeure::PrimitiveProcdeure(const string &name, const procedure &proc): 
-	_namePtr(std::make_shared<string>(name)), 
-	_proc(std::make_shared<procedure>(proc)) {}
-
-Variable PrimitiveProcdeure::operator() (const Variable &args, Environment &env) const { return (*_proc)(args, env); }
-
-string PrimitiveProcdeure::getName() const { return *_namePtr; }
-
-ostream &operator<<(ostream &out, const PrimitiveProcdeure &prim)
-{
-	if (*prim._namePtr == "")
-		out << "#<procedure>";
-	else
-		out << "#<procedure:" + *prim._namePtr + ">";
-	return out;
-}
-
-/* compound procedure */
-
-CompoundProcedure::CompoundProcedure(const Variable &name, const Variable &args, const Variable &seq, const Environment &env):
-	_namePtr(std::make_shared<string>(string(name))),
-	_argsPtr(std::make_shared<Variable>(args)), 
-	_seqPtr(std::make_shared<Variable>(seq)), 
-	_envPtr(std::make_shared<Environment>(env)) {}
-
-CompoundProcedure::CompoundProcedure(const Variable &args, const Variable &seq, const Environment &env):
-	CompoundProcedure(Variable(""), args, seq, env) {}
-
-Variable CompoundProcedure::getSequence() const { return *_seqPtr; }
-
-Variable CompoundProcedure::getArguments() const { return *_argsPtr; }
-
-Environment CompoundProcedure::getEnvironmrnt() const { return *_envPtr; }
-
-void CompoundProcedure::finalize() 
-{ 
-	_envPtr.reset(); 
-	_namePtr.reset(); 
-	_seqPtr.reset();
-	_argsPtr.reset();
-}
-
-ostream &operator<<(ostream &out, const CompoundProcedure &comp)
-{
-	if (*comp._namePtr == "")
-		out << "#<procedure>";
-	else
-		out << "#<procedure:" + *comp._namePtr + ">";
-	return out;
-}
-
-/* variable */
-
-// static member variable
-
-#ifdef EVAL_TRACE_MEMORY
-int Variable::_valueCreated = 0;
-int Variable::_valueDestroyed = 0;
-#endif
-
-const vector<string> Variable::_typeNames = { "void", "null", "number", "string", "symbol", "pair", "procedure", "procedure" };
-
-std::ostream& operator<<(std::ostream& out, const Variable& var)
-{
-	switch (var._type) {
-		case Variable::NUMBER:
-			out << *var._numPtr;
+	switch (var.type) {
+		case Variable::RATIONAL:
+			out << *(var.rationalPtr);
+			break;
+		case Variable::DOUBLE:
+			out << *(var.doublePtr);
+			break;
+		case Variable::STRING:
+			out << '"' << *(var.stringPtr) << '"';
 			break;
 		case Variable::SYMBOL:
-		case Variable::STRING:
-			out << *var._strPtr;
-			break;
-		case Variable::PAIR:
-		case Variable::NIL:
-			out << '(';
-			Variable::printList(out, var);
-			out << ')';
-			break;
-		case Variable::PRIM:
-			out << *var._primPtr;
-			break;
-		case Variable::COMP:
-			out << *var._compPtr;
+			out << *(var.stringPtr);
 	}
 	return out;
 }
 
-void Variable::printList(std::ostream& out, const Variable &var)
+// Arithmetic operations
+Variable operator+(const Variable& lhs, const Variable& rhs)
 {
-	if (var._type != Variable::PAIR)
-		return;
-	const Variable &a = var.car();
-	const Variable &b = var.cdr();
-	out << a;
-	if (b._type == PAIR) {		// print item
-		out << ' ';
-		printList(out, b);
-	} else if (b._type == NIL) {// reach the end of list
-		return;
-	} else {					// not a list
-		out << " . " << b;
-	}
+
 }
 
-bool Variable::isEqual(const Variable &b) const
+Variable operator-(const Variable& lhs, const Variable& rhs)
 {
-	if (_type != b._type)
-		return false;
-	switch (_type) {
-		case Variable::NUMBER:
-			return *_numPtr == *b._numPtr;
-		case Variable::SYMBOL:
-		case Variable::STRING:
-			return *_strPtr == *b._strPtr;
-		case Variable::PAIR:
-			return car().isEqual(b.car()) && cdr().isEqual(b.cdr());
-		case Variable::NIL:
-			return _type == b._type;
-		default:
-			throw SchemeException(string("variable: ") + toString() + "is uncomparable.");
-	}	
+
 }
 
-void Variable::swap(Variable &a, Variable &b)
+Variable operator*(const Variable& lhs, const Variable& rhs)
 {
-	std::swap(a._voidPtr, b._voidPtr);
-	std::swap(a._refCount, b._refCount);
-	std::swap(a._type, b._type);
-	std::swap(a._tag, b._tag);
+
 }
 
-Variable::~Variable()
+Variable operator/(const Variable& lhs, const Variable& rhs)
 {
-	(*_refCount)--;
-	if (*_refCount == 0) {
-		delete _refCount;
-		switch (_type) {
-			case Variable::NUMBER:
-				delete _numPtr;
-				VAL_DESTROYED;
-				break;
-			case Variable::SYMBOL:
-			case Variable::STRING:
-				delete _strPtr;
-				VAL_DESTROYED;
-				break;
-			case Variable::PAIR:
-				delete _pairPtr;
-				delete _tag;
-				VAL_DESTROYED;
-				break;
-			case Variable::PRIM:
-				delete _primPtr;
-				VAL_DESTROYED;
-				break;
-			case Variable::COMP:
-				delete _compPtr;
-				delete _tag;
-				VAL_DESTROYED;
-		}
-	}
+
 }
 
-void Variable::requireType(const std::string &caller, Type type) const
+Variable operator-(const Variable& var)
 {
-	if (_type != type) 
-		throw SchemeException(caller + ": contract violation\n\texpected: " + _typeNames[type] + "\n\tgiven: " + toString());
+
 }
 
-Variable::operator bool() const 
-{ 
-	return _type != SYMBOL || *_strPtr != "false"; 
-}
-
-Variable::operator number() const
+// Logical operations
+Variable operator and(const Variable& lhs, const Variable& rhs)
 {
-	if (!isNumber())
-		throw SchemeException(string("variable: can't convert ") + toString() + " to number");
-	return *_numPtr;
+	if (lhs != VAR_FALSE && rhs != VAR_FALSE)
+		return VAR_TRUE;
+	return VAR_FALSE;
 }
 
-Variable::operator string() const
+Variable operator or(const Variable& lhs, const Variable& rhs)
 {
-	if (!isSymbol() && !isString())
-		throw SchemeException(string("variable: can't convert ") + toString() + " to string");
-	return *_strPtr;
+	if (lhs != VAR_FALSE || rhs != VAR_FALSE)
+		return VAR_TRUE;
+	return VAR_FALSE;
 }
 
-Variable::operator CompoundProcedure() const
+Variable operator xor(const Variable& lhs, const Variable& rhs)
 {
-	if (!isComp())
-		throw SchemeException(string("variable: can't convert ") + toString() + " to compound procedure");
-	return *_compPtr;
+
 }
 
-Variable Variable::operator() (const Variable &var, Environment &env) const
+Variable operator not(const Variable& var)
 {
-	if (!isPrim())
-		throw SchemeException(string("variable: ") + toString() + " isn't a primitive procedure.");
-	return (*_primPtr)(var, env);
+	if (var == VAR_FALSE)
+		return VAR_TRUE;
+	return VAR_FALSE;
 }
 
-Variable Variable::setCar(const Variable &var) 
-{ 
-	requireType("set-car!", PAIR); 
-	_pairPtr->first = var; 
-	return VAR_VOID; 
-}
-
-Variable Variable::setCdr(const Variable &var)
-{ 
-	requireType("set-cdr!", PAIR); 
-	_pairPtr->second = var; 
-	return VAR_VOID; 
-}
-
-// garbage collection
-
-void Variable::setTag(int tag)
+// Compare operations
+Variable operator<(const Variable& lhs, const Variable& rhs)
 {
-	// only PAIR and COMP need tag
-	if (_type != PAIR && _type != COMP)
-		return;
-	// prevent set tag again
-	if (*_tag == tag)
-		return;
-	// set tag
-	*_tag = tag;
-	if (_type == PAIR) {	// PAIR
-		car().setTag(tag);
-		cdr().setTag(tag);
-	} else {				// COMP
-		_compPtr->getSequence().setTag(tag);
-		_compPtr->getArguments().setTag(tag);
-		_compPtr->getEnvironmrnt().setTag(tag);
-	}
+
 }
 
-void Variable::finalize()
+Variable operator>(const Variable& lhs, const Variable& rhs)
 {
-	if (_type == PAIR) {
-		setCar(VAR_NULL);
-		setCdr(VAR_NULL);
-	} else if (_type == COMP) {
-		_compPtr->finalize();
-	}
+
 }
 
-/* environment */
-
-void Environment::addVars(const Variable &vars, const Variable &vals)
+bool operator==(const Variable& lhs, const Variable& rhs)
 {
-	for (Variable varIt = vars, valIt = vals; !varIt.isNull() && !valIt.isNull(); varIt = varIt.cdr(), valIt = valIt.cdr()) {
-		Variable var = varIt.car();
-		Variable val = valIt.car();
-		(*_envPtr)[string(var)] = val;
-	}
+	return lhs.refCount == rhs.refCount;
 }
 
-Environment::Environment(const Variable &vars, const Variable &vals): 
-		_encloseEnvPtr(nullptr), _envPtr(std::make_shared<map<string, Variable>>())
+bool operator!=(const Variable& lhs, const Variable& rhs)
 {
-	addVars(vars, vals);
+	return !(lhs == rhs);
 }
 
-Environment::Environment(const Variable &vars, const Variable &vals, const Environment &encloseEnv): 
-		_encloseEnvPtr(std::make_shared<Environment>(encloseEnv)), _envPtr(std::make_shared<map<string, Variable>>())
+Variable operator<=(const Variable& lhs, const Variable& rhs)
 {
-	addVars(vars, vals);
+
 }
 
-Variable Environment::defineVariable(const Variable &var, const Variable &val)
+Variable operator>=(const Variable& lhs, const Variable& rhs)
 {
-	(*_envPtr)[string(var)] = val;
-	return VAR_VOID;
+
 }
 
-map<string, Variable>::iterator Environment::findVar(const Variable &var)
+Variable Variable::operator()(const Variable& arg, const Environment &env)
 {
-	string str = static_cast<string>(var);
-	for (Environment *envIt = this; envIt; envIt = envIt->_encloseEnvPtr.get()) {
-		auto it = envIt->_envPtr->find(str);
-		if (it != envIt->_envPtr->cend())
-			return it;
-	}
-	throw SchemeException(str + ": variable not found");
-}
 
-Variable Environment::assignVariable(const Variable &var, const Variable &val)
-{
-	auto it = findVar(var);
-	it->second = val;
-	return VAR_VOID;
 }
-
-Variable Environment::lookupVariable(const Variable &var)
-{
-	auto it = findVar(var);
-	return it->second;
-}
-
-void Environment::setTag(int tag)
-{
-	for (auto it = _envPtr->begin(); it != _envPtr->end(); it++)
-		it->second.setTag(tag);
-}
-
-void Environment::finalize()
-{
-	for (auto it = _envPtr->begin(); it != _envPtr->end(); it++)
-		it->second = VAR_NULL;
-}
-
-EVAL_NAMESPACE_END
--- src/variable.hpp
+++ src/variable.hpp
@@ -1,197 +1,222 @@
-#ifndef VARIABLE_H
-#define VARIABLE_H
+//
+// Low level variable and environment for Scheme
+//
+// Author: Zhang Zhenghao (zhangzhenghao@hotmail.com)
+//
+#pragma once
 
+#include <string>
+#include <memory>
+#include <sstream>
 #include <iostream>
 #include <functional>
-#include <memory>
-#include <string>
-#include <vector>
-#include <map>
-#include <utility>
-#include <sstream>
-#include <random>
-#include "evaldef.hpp"
+#include <boost/multiprecision/cpp_int.hpp>
 #include "exception.hpp"
-
-EVAL_NAMESPACE_BEGIN
-
-/* declare */
-
-class Variable;
-class Environment;
-
-/* alias */
-
-template <typename T> using shared_ptr = std::shared_ptr<T>;
-template <typename T> using vector = std::vector<T>;
-template <typename T> using uniform_int_distribution = std::uniform_int_distribution<T>;
-template <typename K, typename V> using map = std::map<K, V>;
-using default_random_engine = std::default_random_engine;
-using number = int;
-using pair = std::pair<Variable, Variable>;
-using ostream = std::ostream;
-using string = std::string;
-using procedure = std::function<Variable(const Variable&, Environment&)>;
-
-/* dynamic object tracer */
-
-#ifdef EVAL_TRACE_MEMORY
-
-#define VAL_CREATED			do { _valueCreated++; } while (0)
-#define VAL_DESTROYED		do { _valueDestroyed++; VAL_DEBUG_STATE; } while (0)
-#define VAL_DEBUG_STATE		do { std::clog << "trace memory: created(" << Variable::_valueCreated\
-								<< ") destroyed(" << Variable::_valueDestroyed\
-								<< ") alive(" << Variable::_valueCreated - Variable::_valueDestroyed << ")" << std::endl; } while(0)
-
-#else
-
-#define VAL_CREATED			do {} while (0)
-#define VAL_DESTROYED		do {} while (0)
-
-#endif
-
-/* garbage collector */
-
-class GarbageCollector
-{
-	static default_random_engine engine;
-	static uniform_int_distribution<int> distribution;
-	static vector<Variable> _watchList;
-	static Environment _globalEnv;
-public:
-	static void setGlobalEnvironment(const Environment& env);
-	static void addVar(const Variable& var);
-	static void collect();
-};
-
-/* primitive procedure */
-
-class PrimitiveProcdeure
-{
-	friend ostream &operator<<(ostream &out, const PrimitiveProcdeure &prim);
-	shared_ptr<string> _namePtr;
-	shared_ptr<procedure> _proc;
-public:
-	PrimitiveProcdeure(const string &name, const procedure &proc);
-	Variable operator() (const Variable &args, Environment &env) const;
-	string getName() const;
-};
-
-/* compound procedure */
-
-class CompoundProcedure
-{
-	friend ostream &operator<<(ostream &out, const CompoundProcedure &comp);
-	shared_ptr<string> _namePtr;
-	shared_ptr<Environment> _envPtr;
-	shared_ptr<Variable> _seqPtr, _argsPtr;
-public:
-	CompoundProcedure(const Variable &name, const Variable &args, const Variable &seq, const Environment &env);
-	CompoundProcedure(const Variable &args, const Variable &seq, const Environment &env);
-	Variable getSequence() const;
-	Variable getArguments() const;
-	Environment getEnvironmrnt() const;
-	void finalize();
-};
-
-/* special variable */
-
-class SpecialVariable {};
-
-/* variable */
 
 class Variable
 {
 public:
-	#ifdef EVAL_TRACE_MEMORY
-	static int _valueCreated;
-	static int _valueDestroyed;
-	#endif
-	static const vector<string> _typeNames;
-	enum Type { VOID, NIL, NUMBER, SYMBOL, STRING, PAIR, PRIM, COMP };
-	friend GarbageCollector;
-	friend std::ostream& operator<<(std::ostream& out, const Variable& var);
-private:
-	// private member variable
-	union {
-		void *_voidPtr;
-		number *_numPtr;
-		string *_strPtr;
-		pair *_pairPtr;
-		PrimitiveProcdeure *_primPtr;
-		CompoundProcedure *_compPtr;
+
+	// Type of variable
+	enum Type {
+		RATIONAL,
+		DOUBLE,
+		STRING,
+		SYMBOL,
+		SPEC,
+		PAIR
 	};
-	int *_refCount;
-	Type _type;
-	// garbage collection
-	int *_tag;
+	
 private:
-	static void swap(Variable &a, Variable &b);
-	static void printList(std::ostream& out, const Variable &var);
+
+	// Indent class
+	class Primitive;
+	class Compound;
+	class Environment;
+
+	// Type alias
+	using string = std::string;
+	using ostream = std::ostream;
+	using ostringstream = std::ostringstream;
+	using pair = std::pair<Variable, Variable>;
+	using cpp_rational = boost::multiprecision::cpp_rational;
+
+	// Type of variable
+	Type type;
+
+	// Value of variable
+	union {
+		cpp_rational*	rationalPtr;
+		double*		doublePtr;
+		string*		stringPtr;
+		pair*		pairPtr;
+		void*		voidPtr;
+	};
+
+	// Reference count
+	int* refCount = nullptr;
+
 public:
-	// constructors
-	Variable(): Variable(SpecialVariable(), NIL) {}
-	Variable(const SpecialVariable &special, Type type): 
-		_voidPtr(nullptr), _type(type), _refCount(new int(1)) 
-	{ if (type != NIL && type != VOID) throw SchemeException("variable: special variable must be null or void"); }
-	Variable(number num): 
-		_numPtr(new number(num)), _type(NUMBER), _refCount(new int(1)) { VAL_CREATED; }
-	Variable(const string &str, Type type = SYMBOL): 
-		_strPtr(new string(str)), _type(type), _refCount(new int(1)) 
-	{ if (type != SYMBOL && type != STRING)	throw SchemeException("variable: variable from string must be string or symbol"); VAL_CREATED; }
-	Variable(const PrimitiveProcdeure &prim): 
-		_primPtr(new PrimitiveProcdeure(prim)), _type(PRIM), _refCount(new int(1)) { VAL_CREATED; }
-	Variable(const Variable &name, const Variable &args, const Variable &seq, const Environment &env):
-		_compPtr(new CompoundProcedure(name, args, seq, env)), _type(COMP), _refCount(new int(1)), _tag(new int) { VAL_CREATED; GarbageCollector::addVar(*this); }
-	Variable(const Variable &args, const Variable &seq, const Environment &env):
-		Variable(Variable(""), args, seq, env) { }
-	Variable(const Variable &a, const Variable &b): 
-		_pairPtr(new pair(a, b)), _type(PAIR), _refCount(new int(1)), _tag(new int) { VAL_CREATED; GarbageCollector::addVar(*this); }
-	Variable(const Variable &var): _voidPtr(var._voidPtr), _type(var._type), _refCount(var._refCount), _tag(var._tag) { (*_refCount)++; }
-	Variable& operator=(Variable var) { swap(*this, var); }
-	~Variable();
-	// interface
-	void requireType(const string &caller, Type type) const;
-	string toString() const { std::stringstream ss; ss << (*this); return ss.str(); }
-	bool isNull() const { return _type == NIL; }
-	bool isNumber() const { return _type == NUMBER; }
-	bool isVoid() const { return _type == VOID; }
-	bool isPair() const { return _type == PAIR; }
-	bool isSymbol() const { return _type == SYMBOL; }
-	bool isString() const { return _type == STRING; }
-	bool isPrim() const { return _type == PRIM; }
-	bool isComp() const { return _type == COMP; }
-	bool isEqual(const Variable &var) const;
-	bool isEqual(const string &str) const { return isSymbol() && *_strPtr == str; }
-	Variable car() const { requireType("car", PAIR); return _pairPtr->first; }
-	Variable cdr() const { requireType("cdr", PAIR); return _pairPtr->second; }
-	Variable setCar(const Variable &var);
-	Variable setCdr(const Variable &var);
-	explicit operator bool() const;
-	explicit operator number() const;
-	explicit operator string() const;
-	explicit operator CompoundProcedure() const;
-	Variable operator() (const Variable &var, Environment &env) const;
-	// garbage collector
-	void setTag(int tag);
-	void finalize();
+
+	// Constructor for special
+	Variable(): type(SPEC), refCount(new int(1)) {}
+
+	// Constructor for rational
+	Variable(const cpp_rational& rational): 
+		type(RATIONAL), refCount(new int(1)), rationalPtr(new cpp_rational(rational)) {}
+
+	// Constructor for double
+	Variable(double value):
+		type(DOUBLE), refCount(new int(1)), doublePtr(new double(value)) {}
+
+	// Constructor for rational, double, string and symbol
+	Variable(const string &str, Type type): type(type), refCount(new int(1))
+	{
+		switch (type) {
+			// Convert string to rational
+			case RATIONAL:
+				rationalPtr = new cpp_rational(str);
+				break;
+			// Convert string to double
+			case DOUBLE:
+				doublePtr = new double(std::stod(str));
+				break;
+			case STRING:
+			case SYMBOL:
+				stringPtr = new string(str);
+				break;
+			default:
+				throw Exception("intern error: variable construction error");
+		}
+	}
+
+	// Constructor for pairs
+	Variable(const Variable& lhs, Variable& rhs): 
+		type(PAIR), refCount(new int(1)), pairPtr(new pair(lhs, rhs)) {}
+
+	// Copy constructor
+	Variable(const Variable& var): type(var.type), refCount(var.refCount), voidPtr(var.voidPtr)
+	{
+		// Increase reference count
+		(*refCount)++;
+	}
+
+	// Destructor
+	~Variable() {
+		// Decrease reference count
+		(*refCount)--;
+		if (*refCount > 0)
+			return;
+		// Free memory
+		switch (type) {
+			case RATIONAL:
+				delete rationalPtr;
+				break;
+			case DOUBLE:
+				delete doublePtr;
+				break;
+			case STRING:
+			case SYMBOL:
+				delete stringPtr;
+		}
+	}
+
+	// Assignment
+	Variable& operator=(Variable var)
+	{
+		swap(*this, var);
+		return *this;
+	}
+
+	// Get variable type
+	Type getType() const
+	{
+		return type;
+	}
+
+	// Convert variable to string
+	string toString() const
+	{
+		ostringstream out;
+		out << (*this);
+		return out.str();
+	}
+
+	// Standard output
+	friend ostream& operator<<(ostream& out, const Variable& var);
+
+	// Swap two variables
+	friend void swap(Variable& lhs, Variable& rhs);
+
+	// Arithmetic operations
+	friend Variable operator+(const Variable& lhs, const Variable& rhs);
+	friend Variable operator-(const Variable& lhs, const Variable& rhs);
+	friend Variable operator*(const Variable& lhs, const Variable& rhs);
+	friend Variable operator/(const Variable& lhs, const Variable& rhs);
+	friend Variable operator-(const Variable& var);
+
+	// Logical operations
+	friend Variable andVar(const Variable& lhs, const Variable& rhs);
+	friend Variable orVar(const Variable& lhs, const Variable& rhs);
+	friend Variable xorVar(const Variable& lhs, const Variable& rhs);
+	friend Variable notVar(const Variable& var);
+
+	// Compare operations
+	friend Variable operator<(const Variable& lhs, const Variable& rhs);
+	friend Variable operator>(const Variable& lhs, const Variable& rhs);
+	friend bool operator==(const Variable& lhs, const Variable& rhs);
+	friend bool operator!=(const Variable& lhs, const Variable& rhs);
+	friend Variable operator<=(const Variable& lhs, const Variable& rhs);
+	friend Variable operator>=(const Variable& lhs, const Variable& rhs);
+
+	// Function call
+	Variable operator()(const Variable& arg, const Environment& env);
 };
 
-/* constants */
-
-const Variable VAR_TRUE = Variable("true", Variable::SYMBOL);
-const Variable VAR_FALSE = Variable("false", Variable::SYMBOL);
-const Variable VAR_NULL = Variable(SpecialVariable(), Variable::NIL);
-const Variable VAR_VOID = Variable(SpecialVariable(), Variable::VOID);
-
-/* environment */
-
-class Environment
+// Swap two variables
+inline void swap(Variable& lhs, Variable& rhs)
 {
-	shared_ptr<Environment> _encloseEnvPtr;
-	shared_ptr<map<string, Variable>> _envPtr;
+	std::swap(lhs.type, rhs.type);
+	std::swap(lhs.voidPtr, rhs.voidPtr);
+	std::swap(lhs.refCount, rhs.refCount);
+}
+
+class Variable::Primitive
+{
+	string name;	// Name of procedure
+
+public:
+	Primitive();
+	~Primitive();
+};
+
+class Variable::Compound
+{
+	string name;	// Name of procedure
+
+public:
+	Compound();
+	~Compound();
+};
+
+// Constant values
+const Variable VAR_NULL		= Variable();
+const Variable VAR_VOID		= Variable();
+const Variable VAR_TRUE		= Variable();
+const Variable VAR_FALSE	= Variable();
+
+/*class Environment
+{
+	// Type alias
+	using frame = std::map<Variable, Variable>;
+	template <typename T> using shared_ptr = std::shared_ptr<T>;
+
+	// Data member
+	shared_ptr<Environment> encloseEnvPtr;
+	shared_ptr<frame> framePtr;
+
 	void addVars(const Variable &vars, const Variable &vals);
-	map<string, Variable>::iterator findVar(const Variable &var);
+	frame::iterator findVar(const Variable &var);
 public:
 	Environment(const Variable &vars = VAR_NULL, const Variable &vals = VAR_NULL);
 	Environment(const Variable &vars, const Variable &vals, const Environment &encloseEnv);
@@ -200,8 +225,4 @@ public:
 	Variable lookupVariable(const Variable &var);
 	void setTag(int tag);
 	void finalize();
-};
-
-EVAL_NAMESPACE_END
-
-#endif
+};*/
