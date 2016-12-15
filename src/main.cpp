4985cc153d7b0e2d420bab48d5c7fdc084c561b8
ZhangZhenghao <zhangzhenghao@hotmail.com>
2016-12-15 22:02:42 +0800

    Implemented main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "variable.hpp"
 #include "primitive.hpp"
 #include "evaluator.hpp"
@@ -7,8 +8,9 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+int console()
 {
+	cout << "Welcome to Simple Scheme v0.1" << endl;
 	Variable var;
 	// Setup initial environment
 	Environment env = Primitive::setupEnvironment();
@@ -26,6 +28,40 @@ int main(int argc, char const *argv[])
 		#ifdef STATS
 		Statistic::printStatistic();
 		#endif
+	}
+	return 0;
+}
+
+int file(istream& in)
+{
+	Variable var;
+	// Setup initial environment
+	Environment env = Primitive::setupEnvironment();
+	while (in >> var) {
+		try {
+			Variable ret = Evaluator::eval(var, env);
+			if (ret != VAR_VOID)
+				cout << ret << endl;
+		} catch (Exception e) {
+			e.printStack();
+		}
+		// Collect garbage
+		GarbageCollector::collect(env);
+		// Print statistic information
+		#ifdef STATS
+		Statistic::printStatistic();
+		#endif
+	}
+	return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+	if (argc > 1) {
+		ifstream fin(argv[1]);
+		file(fin);
+	} else {
+		console();
 	}
 	return 0;
 }
