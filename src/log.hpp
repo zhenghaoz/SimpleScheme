e89648d8b5802e9ba1d543d5a3fb42bfeadfd338
ZhangZhenghao <zhangzhenghao@hotmail.com>
2016-12-15 21:55:29 +0800

    Implemented logger

---
+++ src/log.hpp
@@ -0,0 +1,10 @@
+// 
+// Scheme logger
+// 
+// Author: Zhang Zhenghao (zhangzhenghao@hotmail.com)
+//
+#include <iostream>
+
+#define VERBOSE(tag,msg)	(std::clog<<"\x1B[1;36m"<<tag<<':'<<msg<<"\x1B[0m"<<std::endl)
+#define ERROR(tag,msg)		(std::clog<<"\x1B[1;35m"<<tag<<':'<<msg<<"\x1B[0m"<<std::endl)
+#define DEBUG(tag,msg)		(std::clog<<"\x1B[1;34m"<<tag<<':'<<msg<<"\x1B[0m"<<std::endl)
